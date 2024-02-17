134e0f102a21555f1ef4a564a6972fd1853f6bbb
Tom French <hello@orseti.com>
2024-02-17 22:47:15 +0000

    fx

--- src/arch/riscv64/mod.rs
+++ src/arch/riscv64/mod.rs
@@ -1,7 +1,6 @@
-use crate::{arch::mm::alloc::MaqAllocator, println};
+use crate::println;
 
 pub mod boot;
-pub mod mm;
 pub mod trap;
 
 struct RiscV64;
@@ -10,24 +9,17 @@ impl super::Arch for RiscV64 {
     #[no_mangle]
     extern "C" fn kinit() {
         println!("Walnut initializing...");
-        let alloca = MaqAllocator::new();
-        unsafe {
-            if let Some(ptr) = alloca.alloc_pages(64) {
-                println!("Found and allocated pages at {:0p}", ptr);
-                alloca.print_page_table();
-                mm::init_mm(&alloca);
-            } else {
-                println!("Unable to find space to allocate {} pages for kernel!", 64);
-                panic!();
-            }
-            core::arch::asm!("nop;nop;")
-        }
+        unsafe { core::arch::asm!("nop;nop;") }
     }
 }
 
 impl RiscV64 {
     #[no_mangle]
     extern "C" fn kinit_hart() {
-        unsafe { core::arch::asm!("nop;nop;") }
+        let hart_id: usize;
+        unsafe {
+            core::arch::asm!("csrr {}, mhartid", out(reg) hart_id);
+        }
+        println!("Hello from hart thread {} ", hart_id);
     }
 }
